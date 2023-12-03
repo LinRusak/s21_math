ee07a5468b5ec406d572cc6fc55ca37fd7820419
Blancasa <blancasa@student.21-school.ru>
2023-12-03 12:56:31 +0700

    func&test

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-#define S21_EPS 1e-200
+#define S21_EPS 1e-100
 #define S21_PI 3.1415926535897932
 #define S21_EXP 2.7182818284590452
 #define S21_MAX 1e100
@@ -18,6 +18,13 @@ long double s21_asin(double x);
 long double s21_atan(double x);
 long double s21_ceil(double x);
 long double s21_cos(double x);
+long double s21_exp(double x);
 long double s21_fabs(double x);
 long double s21_floor(double x);
 long double s21_fmod(double x, double y);
+long double s21_log(double x);
+long double s21_pow(double base, double exp);
+long double s21_sin(double x);
+long double s21_sqrt(double x);
+long double s21_tan(double x);
+long double s21_factorial(int x);
