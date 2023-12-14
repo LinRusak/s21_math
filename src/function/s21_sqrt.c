4c651a12475b7183c8892cacb4bdb6ddd351853b
Blancasa <blancasa@student.21-school.ru>
2023-12-14 13:30:47 +0700

    нет тестов и не собирается

--- src/function/s21_sqrt.c
+++
@@ -1,3 +0,0 @@
-#include "s21_math.h"
-
-long double s21_sqrt(double x) { return s21_pow(x, 0.5); }
--- src/s21_math.h
+++ src/s21_math.h
@@ -1,30 +1,30 @@
-#include <stdint.h>
-#include <stdio.h>
-#define S21_EPS 1e-100
-#define S21_PI 3.1415926535897932
-#define S21_EXP 2.7182818284590452
-#define S21_MAX 1e100
-#define S21_NAN (__builtin_nanf(""))
-#define POS_INF 1.0 / 0.0L
-#define NEG_INF -1.0 / 0.0L
-#define isNAN(x) (x != x)
-#define isPOS_INF(x) (x == POS_INF)
-#define isNEG_INF(x) (x == NEG_INF)
-#define isNormal(x) (!isNAN(x) && !isNEG_INF(x) && !isPOS_INF(x))
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
 
-int s21_abs(int x);
-long double s21_acos(double x);
-long double s21_asin(double x);
-long double s21_atan(double x);
-long double s21_ceil(double x);
-long double s21_cos(double x);
-long double s21_exp(double x);
-long double s21_fabs(double x);
-long double s21_floor(double x);
-long double s21_fmod(double x, double y);
-long double s21_log(double x);
-long double s21_pow(double base, double exp);
+#define S21_LLONG_MAX __LONG_LONG_MAX__
+#define S21_LLONG_MIN (-__LONG_LONG_MAX__ - 1)
+#define S21_E 2.71828182845904523536028747135266250
+#define S21_M_PI \
+  3.1415926535897932384626433832795028841971693993751058209749445923078164
+#define S21_NAN 0.0 / 0.0
+#define S21_INF 1.0 / 0.0
+#define EPS_6 1e-06
+
+long double s21_pow(double base, double expi);
 long double s21_sin(double x);
 long double s21_sqrt(double x);
 long double s21_tan(double x);
-long double s21_factorial(int x);
+long double s21_floor(double num);
+long double s21_log(double num);
+long double s21_fmod(double num_1, double num_2);
+long double s21_trunc(double x);
+long double s21_cos(double x);
+long double s21_fabs(double x);
+long double s21_ceil(double x);
+long double s21_exp(double x);
+int s21_abs(int a);
+long double s21_acos(double x);
+long double s21_asin(double a);
+long double s21_atan(double x);
+
+#endif
