89b5e1255536318db48e20148c8f65c761b145ba
Goodbrom <trojan226den@gmail.com>
2023-12-24 22:56:46 +0700

    all work

--- src/function/s21_atan.c
+++ src/function/s21_atan.c
@@ -5,5 +5,5 @@ long double s21_atan(double x) {
     if(x != x || x >= S21_LLONG_MAX || x <= S21_LLONG_MIN){
         return (x != x) ? x : S21_NAN;
     }
-    return s21_xsin(x / s21_sqrt(1.0 + x * x)); 
+    return s21_asin(x / s21_sqrt(1.0 + x * x)); 
 }
